df7def237ff07590815be40d9b000063df6890ca
youngriming <youngriming@gmail.com>
2023-01-25 17:45:40 +0900

    Day2 AWS Posts Save Posts update Posts findById PostsApiControllerTest

---
+++ src/main/java/org/mega/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,41 @@
+package org.mega.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+    private String author;
+    /*
+    * @Builder
+    * 해당 클래스의 빌더 패턴 클래스 생성, 클래스 안에 클래스 만드는 것 = 내부 클래스 = 빌더패턴 (보안 향상을 위해서)
+    * 생성자 상단에 선언 시 생성자에 포한된 필드만 빌더에 포함
+    * 객체를 생성하는 클래스와 표현하는 클래스를 분리/ 서로 다른 표현으로 제공
+    */
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title=title;
+        this.content=content;
+        this.author=author;
+    }
+    public void update(String title, String content){
+        this.title=title;
+        this.content=content;
+    }
+}
---
+++ src/main/java/org/mega/book/springboot/service/PostsService.java
@@ -0,0 +1,36 @@
+package org.mega.book.springboot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.mega.book.springboot.domain.posts.Posts;
+import org.mega.book.springboot.domain.posts.PostsRepository;
+import org.mega.book.springboot.web.dto.PostsResponseDto;
+import org.mega.book.springboot.web.dto.PostsSaveRequestDto;
+import org.mega.book.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+        return new PostsResponseDto(entity);
+    }
+
+}
